b32414d6ec82830910ccad0e1ceb52773db8f624
Rosemale-John II C. Villacorta <rosemalejohn@gmail.com>
2016-10-06 22:46:25 +0800

    Add migrations

---
+++ database/migrations/2016_10_06_143256_create_files_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('url');
+            $table->string('filename');
+            $table->string('description');
+            $table->string('mimetype');
+            $table->integer('size');
+            $table->integer('category_id')->unsigned();
+            $table->integer('department_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
