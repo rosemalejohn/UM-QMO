01f78ab731e92693a79307d3a7694ec6e3c45c5c
Rosemale-John II C. Villacorta <rosemalejohn@gmail.com>
2016-10-09 22:26:03 +0800

    Fix redirect to home bug

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
  */
 
-Route::group(['middleware' => 'guest'], function () {
+Route::group(['middleware' => 'auth'], function () {
 
     Route::get('/', function () {
         return view('layouts.app');
